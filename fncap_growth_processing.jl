11bf00ebde471caa0023e7c260a94e8308c99ae0
Andrew Steinkruger <amsteinkruger@gmail.com>
2025-02-13 14:26:54 -0800

    Through draws for modeling, plus housekeeping.

--- fncap_growth_processing.jl
+++ fncap_growth_processing.jl
@@ -2,8 +2,13 @@
 
 using Tidier
 using Random
+using Plots
+using FastHalton
+using Distributions
 
 # Seed
+
+#  Question: is this pointless because Julia requires seeds to be defined by function call (unlike MATLAB and R)?
 
 Random.seed!(112358)
 
@@ -25,5 +30,24 @@ plot(dat_growth.Volume_First, dat_growth.Volume_Difference, seriestype=:scatter)
 
 # Initialize parameters.
 
-b0 = [100, 1, 1000, 0.5]
+#  Question: is there a good reason to define b_0 sooner rather than later? Might fit better in the minimization block.
 
+b_0 = [100, 1, 1000, 0.5]
+t_0 = 1
+T_0 = 100
+n_0 = 1000
+
+# Get draws.
+
+# Questions:
+#  - Does the base in HaltonSeq matter?
+#  - Does the absence of MATLAB's "leap" argument matter?
+#  - Am I confusing the T and n arguments?
+#  - Would pre-allocation be useful?
+
+Base_Halton = 3
+Draws_Halton = HaltonSeq(Base_Halton, T_0, n_0)
+Vec_Halton = collect(Draws_Halton)
+Out_Halton = Quantile(Normal(0, 1), Vec_Halton)
+
+# Minimize.
