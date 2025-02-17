c308cd91d561983a211f185334af2f9c3d958b52
amsteinkruger <amsteinkruger@gmail.com>
2025-02-16 16:48:19 -0800

    Through model estimation for a single site class.

--- fncap_growth_model.jl
+++ fncap_growth_model.jl
@@ -1,6 +1,7 @@
 # Estimate a model or models for growth data from Douglas fir plots in FIA DB.
 
-# So far: got a function to minimize. Next: minimization, functional programming for multiple models, visualization.
+# Done: define and minimize a growth function for a particular site productivity class.
+# To do: transition into functional programming to estimate a model for each site class.
 
 # Packages
 
@@ -27,26 +28,28 @@ plot(dat_growth.Volume_First, dat_growth.Volume_Second, seriestype=:scatter)
 
 plot(dat_growth.Volume_First, dat_growth.Volume_Difference, seriestype=:scatter)
 
+# Set up inputs to a growth function for minimization.
+
 # Set parameters. 
 
-b = [100, 1, 1000, 0.5]
+b = [100, 1, 100, 0.5]
 t = 1
 T = maximum(dat_growth.Year_Difference)
 skip = 1000
-
-# Get a function to minimize (actually do a bunch of other stuff first).
 
 # Get data. 
 
 dat_growth_3 = @filter(dat_growth, SITECLCD == 3) 
 
 # Get length.
+
 par_count = nrow(dat_growth_3)
 
 # Initialize a data object for simulation.
+
 W_sim = zeros(par_count, T)
 W_sim[:, 1] = reshape(dat_growth_3.Volume_First, par_count, 1)
-# W_sim[:, 1] = W_sim[:, 1] .+ b_1 # This follows the .m.
+# W_sim[:, 1] = W_sim[:, 1] .+ b_1 # This follows the .m in initializing growth with a parameter.
 
 # Get outcomes for comparison.
 
@@ -70,7 +73,7 @@ dat_noise = fun_halton(3, 0, 1, par_count, T, skip)
 
 # Get function to minimize.
 
-function fun_growth(b, t, T, par_count, initialization, noise, outcome)
+function fun_growth(b, t, T, initialization, noise)
 
     # Declare objects. This works, but doesn't seem right.
     b_1 = b[1]
@@ -79,14 +82,8 @@ function fun_growth(b, t, T, par_count, initialization, noise, outcome)
     b_4 = b[4]
     t = t
     T = T
-
-    # declare objects with stupid names
     W_sim = initialization
     Out_Halton_Matrix = noise
-
-    # declare an object because ...
-    # w_t = zeros(par_count, 1)
-    # u_t = zeros(par_count, 1)
 
     # Run the simulation.
     for i in 2:T
@@ -98,55 +95,54 @@ function fun_growth(b, t, T, par_count, initialization, noise, outcome)
     # Sum values up to the full period of interest. 
     W_sim2 = sum(W_sim, dims = 2)
 
-    # Get residuals.
-    W_out = outcome - W_sim2
-
-    # return(W_out)
-
-    # Get the sum of squared residuals?
-    W_sq = W_out .^ 2
-    W_sum_sq = sum(W_sq)
-
-    return(W_sum_sq)
+    return(W_sim2)
 
 end
 
 # Test function with initial values.
 
-fun_growth(b, t, T, par_count, W_sim, dat_noise, dat_growth_3_end)
+fun_growth(b, t, T, W_sim, dat_noise)
 
-# Minimize.
-
-# demo
-
-f(x) = (1.0 - x[1])^2 + 100.0 * (x[2] - x[1]^2)^2
-
-optimize(f, [0.0, 0.0])
-
-# set up another function to pass parameters because that's idiomatic, I guess
-# https://stackoverflow.com/questions/26158142/pass-additional-arguments-to-a-built-in-function
+# Set up a wrapper function for minimization.
 
 function fun_growth_wrapper(b)
     b = b
-    fun_growth(b, t, T, par_count, W_sim, dat_noise, dat_growth_3_end) # object names are downright tragic
+    dat_out = fun_growth(b, t, T, W_sim, dat_noise) 
+
+    out = sum((dat_growth_3_end - dat_out) .^ 2) # Mind the global reference to dat_growth_3_end.
+
 end
 
-# Test wrapper function with initial values (this is silly, needs revision for order)
-
-fun_growth_wrapper(b)
-
-# actually minimize
+# Minimize the wrapper function. 
 
 dat_results = optimize(fun_growth_wrapper, b)
 
-# get parameters back out of minimization
+# Back parameters out of minimization.
 
 dat_b = Optim.minimizer(dat_results)
 
-dat_sum_sq = Optim.minimum(dat_results) # that's a big fucking number
+dat_sum_sq = Optim.minimum(dat_results)
 
-# Visualize?
+# Visualize
 
-# Compare to polynomials of order 1, 2, 3?
+#  Set up single dataframe of observations and predictions. This could be tidier.
 
-# turn into functional programming approach for each site productivity class
+dat_predictions_col = fun_growth(dat_b, t, T, W_sim, dat_noise)
+
+dat_predictions = copy(dat_growth_3)
+
+dat_predictions[!, :predictions] = vec(dat_predictions_col)
+
+dat_predictions[!, :Volume_Difference_Predicted] = dat_predictions.predictions - dat_predictions.Volume_First
+
+#  Plot second volume on first volume.
+
+plot(dat_predictions.Volume_First, dat_predictions.Volume_Second, seriestype=:scatter)
+plot!(dat_predictions.Volume_First, dat_predictions.predictions, seriestype=:scatter)
+
+#  Plot growth on first volume.
+
+plot(dat_predictions.Volume_First, dat_predictions.Volume_Difference, seriestype=:scatter)
+plot!(dat_predictions.Volume_First, dat_predictions.Volume_Difference_Predicted, seriestype=:scatter)
+
+# Transition into functional programming to estimate a model for each site productivity class.
