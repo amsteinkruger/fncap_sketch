776372fea5e7232c153bdf4415c7fe60cd1d3832
amsteinkruger <amsteinkruger@gmail.com>
2025-04-08 16:04:19 -0700

    Tidied up, also broke something (?!).

---
+++ 00_toy.jl
@@ -0,0 +1,144 @@
+# Estimate a toy model on fake data.
+
+# Packages
+
+using Tidier
+using Random
+using Plots
+using FastHalton
+using Distributions
+using Optim
+
+# Set up fake data.
+#  Omit complications: negative growth, uneven counts of periods between observations, . . ..
+
+vec_volume_0 = 
+@chain begin
+    Normal(100, 25)
+    Truncated(0, Inf)
+    rand(100)
+end
+
+vec_volume_1 = 
+@chain begin
+    Normal(100, 25)
+    Truncated(0, Inf)
+    rand(100)
+    + vec_volume_0
+end
+
+dat = DataFrame(volume_0 = vec_volume_0, volume_1 = vec_volume_1)
+dat[!, :volume_d] = dat.volume_1 - dat.volume_0
+
+# Set up parameters. 
+
+b = [1, 100, 0.5]
+t = 1
+T = 10
+skip = 1000
+
+# Get data length.
+
+par_count = nrow(dat) 
+
+# Initialize a data object for simulation.
+
+dat_initial = zeros(par_count, T)
+dat_initial[:, 1] = reshape(dat.volume_0, par_count, 1)
+
+# Get outcomes for comparison.
+
+dat_end = dat.volume_1
+
+# Get noise. 
+
+function fun_halton(base_halton, mean_halton, sd_halton, par_count, T, skip)
+
+    base_halton = base_halton
+    draws_halton = HaltonSeq(base_halton, par_count * T, skip)
+    vector_halton_0 = collect(draws_halton)
+    vector_halton_1 = quantile(Normal(mean_halton, sd_halton), vector_halton_0)
+    matrix_halton = reshape(vector_halton_1, par_count, T)
+
+    matrix_halton
+
+end
+
+dat_noise = fun_halton(3, 0, 1, par_count, T, skip)
+
+# Get a growth function.
+
+function fun_growth(b = b, t = t, T = T, initial = dat_initial, noise = dat_noise)
+
+    # Declare objects. This works, but doesn't seem right.
+    b_1 = b[1]
+    b_2 = b[2]
+    b_3 = b[3]
+    t = t
+    T = T 
+    dat_simulation = initial
+    dat_noise = noise
+
+    # Run the simulation.
+    for i in 2:T
+        w_t = dat_simulation[:, i - 1]
+        u_t = dat_noise[:, i - 1]
+        dat_simulation[:, i] = w_t .* (b_1 ./ (1 .+ ((b_1 - 1) ./ b_2) .* w_t)) .* exp.(b_3 .* u_t .- (1 / 2) * b_3 .^ 2)
+    end
+
+    # Sum values up to the full period of interest. 
+    sum(dat_simulation, dims = 2)
+
+end
+
+# Test the growth function.
+
+fun_growth()
+
+# Get an objective function to wrap the growth function.
+
+function fun_objective(b = b, t = t, T = T, dat_initial = dat_initial, dat_noise = dat_noise, dat_end = dat_end)
+    b = b
+    t = t
+    T = T
+    dat_initial = dat_initial
+    dat_noise = dat_noise
+    dat_end = dat_end
+
+    dat_out = fun_growth(b, t, T, W_sim, dat_noise) 
+
+    sum((dat_end - dat_out) .^ 2)
+
+end
+
+# Test the objective function.
+
+fun_objective()
+
+# Minimize the wrapper function. 
+
+estimate = optimize(fun_objective, b)
+
+# Back parameters out of minimization.
+
+b_hat = Optim.minimizer(estimate)
+
+measure = Optim.minimum(estimate)
+
+# Visualize.
+
+#  Set up single dataframe of observations and predictions. This could be tidier.
+
+dat_predictions = copy(dat)
+dat_predictions[!, :volume_1_hat] = vec(fun_growth(b_hat))
+dat_predictions[!, :volume_d_hat] = dat_predictions.volume_1_hat - dat_predictions.volume_0
+
+#  Plot second volume on first volume.
+
+plot(dat_predictions.volume_0, dat_predictions.volume_1, seriestype=:scatter)
+plot!(dat_predictions.volume_0, dat_predictions.volume_1_hat, seriestype=:scatter)
+
+#  Plot growth on first volume.
+
+plot(dat_predictions.volume_0, dat_predictions.volume_d, seriestype=:scatter)
+plot!(dat_predictions.volume_0, dat_predictions.volume_d_hat, seriestype=:scatter)
