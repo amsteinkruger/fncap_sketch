d7460521ab902d8461a0326df1a2c6ca1d874300
amsteinkruger <amsteinkruger@gmail.com>
2025-04-25 13:49:46 -0700

    Through a first pass at replication.

--- 00_toy_sc.jl
+++ 00_toy_sc.jl
@@ -10,6 +10,7 @@ using Plots
 using FastHalton
 using Distributions
 using Optim
+using LeastSquaresOptim
 
 # Data
 
@@ -18,7 +19,7 @@ using Optim
 dat =
     @chain begin
         "matlab/Growth_Data_SC_clean.csv"
-        read_csv(col_names = false)
+        read_csv(col_names=false)
         rename(Dict("Column1" => "Age", "Column2" => "Volume", "Column3" => "Class"))
         @filter(Class == 4)
         @group_by(Age)
@@ -26,76 +27,83 @@ dat =
         @ungroup
     end
 
+age_max = maximum(dat.Age)
+
 # Halton 
 
-# Seed.
+# Set seed.
 
 Random.seed!(0112358)
 
-## Break for 2025/04/25
+#  Set parameters.
 
-#  Parameters.
-
-base_halton = 3
+base_halton = 2
+dim_1_halton = 10000 # Draws
+dim_2_halton = age_max # Maximum Stand Age
+skip_halton = 1000
 mean_halton = 0
 sd_halton = 1
-draws_halton = 1000
-skip_halton = 1000
 
-# Get noise. 
+#  Get draws. 
 
-function fun_halton(base=base_halton, mean=mean_halton, sd=sd_halton, draws=draws_halton, T=T, skip=skip_halton)
+dat_noise =
+    @chain begin
+        HaltonSeq(base_halton, dim_1_halton * dim_2_halton, skip_halton) # Note docs reverse arguments.
+        collect
+        quantile(Normal(), _)
+        reshape((dim_1_halton, dim_2_halton))
+        shuffle
+    end
 
-    seq = HaltonSeq(base, draws * T, skip)
-    vector_0 = collect(seq)
-    vector_1 = quantile(Normal(mean, sd), vector_0)
-    matrix = reshape(vector_1, (draws, T))
+# Get functions for growth and optimization.
 
-    return (matrix)
+#  Set parameter guesses.
 
-end
+b = [3.0, 1.5, 7.5, 7.5] # Scaling for easier comparison.
 
-dat_noise = fun_halton()
+#  Initialize a data object to loop over.
 
-# Get a growth function.
+dat_initial = zeros(dim_1_halton, dim_2_halton)
 
-#  Parameters.
+#  Set up the growth function.
 
-b = [1, 1, 100, 0.5]
-t = 1
-T = maximum(dat.age_1)
+function fun_growth(b = b, T = age_max, dat_simulation = dat_initial, dat_noise = dat_noise)
 
-dat_initial = zeros(draws_halton, T)
-
-#  Function.
-
-function fun_growth(b=b, t=t, T=T, dat_simulation=dat_initial, dat_noise=dat_noise)
+    b = b .* [1e1, 1e0, 1e3, 1e-1] # This rescales parameter guesses.
 
     dat_simulation[:, 1] .= b[1]
+
     # Run the simulation.
+
     for i in 2:T
         w_t = dat_simulation[:, i-1]
         u_t = dat_noise[:, i-1]
-        dat_simulation[:, i] = w_t .* (b[2] ./ (1 .+ ((b[2] - 1) ./ b[3]) .* w_t)) .* 1 # exp.(b[4] .* u_t .- (1 / 2) * b[4] .^ 2)
+        dat_simulation[:, i] = w_t .* (b[2] ./ (1 .+ ((b[2] - 1) ./ b[3]) .* w_t)) .* exp.(b[4] * u_t .- (1 / 2) * b[4] ^ 2)
     end
+
     # Get means over simulations.
+
     out = mean(dat_simulation, dims=1)
 
-    return (out)
+    return(out)
 
 end
 
-# Test the growth function.
+#  Test the growth function.
 
 fun_growth()
 
-# Get an objective function to wrap the growth function.
+#  Set up the objective function.
 
-function fun_objective(b=b, t=t, T=T, dat_initial=dat_initial, dat_noise=dat_noise, dat=dat)
+function fun_objective(b = b, T = age_max, dat_initial = dat_initial, dat_noise = dat_noise, dat_observed = dat)
 
-    dat_prediction = DataFrame(age_1=1:T, prediction=Array(fun_growth(b, t, T, dat_initial, dat_noise)')[:, 1]) # Awful.
-    dat = leftjoin(dat, dat_prediction, on=:age_1)
-    sum(abs.(dat.volume_1 - dat.prediction))
+    dat_prediction = DataFrame(Age = 1:T, Volume_Hat = Array(fun_growth(b, T, dat_initial, dat_noise)')[:, 1]) # Awful.
+    dat = leftjoin(dat_observed, dat_prediction, on = :Age)
+    out = sum((dat.Volume - dat.Volume_Hat) .^ 2) 
+    # out = sum(abs.(dat.Volume - dat.Volume_Hat))
+    # Other loss functions go here, with a selection function defaulting to sum of squares.
+
+    return(out)
 
 end
 
@@ -105,37 +113,21 @@ fun_objective()
 
 # Minimize the wrapper function. 
 
-estimate = optimize(fun_objective, b)
+estimate = optimize(fun_objective, b, LevenbergMarquardt())
 
 # Back parameters out of minimization.
 
-b_hat = Optim.minimizer(estimate)
-measure = Optim.minimum(estimate)
+b_hat = estimate.minimizer
+measure = estimate.ssr
 
 # Visualize.
 
 #  Set up single dataframe of observations and predictions. This could be tidier.
 
-dat_prediction = DataFrame(age_1=1:T, volume_1_hat=Array(fun_growth(b_hat)')[:, 1])
-dat_out = leftjoin(dat, dat_prediction, on=:age_1)
-dat_out[!, :volume_d_hat] = dat_out.volume_1_hat - dat_out.volume_0
+dat_prediction = DataFrame(Age = 1:age_max, Volume_Hat = Array(fun_growth(b_hat)')[:, 1])
+dat_out = leftjoin(dat, dat_prediction, on =:Age)
 
-#  Plot second volume on first volume.
+#  Plot observed and predicted volumes on stand age.
 
-plot(dat_out.volume_0, dat_out.volume_1, seriestype=:scatter)
-plot!(dat_out.volume_0, dat_out.volume_1_hat, seriestype=:scatter)
-
-#  Plot growth on first volume.
-
-plot(dat_out.volume_0, dat_out.volume_d, seriestype=:scatter)
-plot!(dat_out.volume_0, dat_out.volume_d_hat, seriestype=:scatter)
-
-# Plot second volume on second age.
-
-plot(dat_out.age_1, dat_out.volume_1, seriestype=:scatter)
-plot!(dat_out.age_1, dat_out.volume_1_hat, seriestype=:scatter)
-
-#  Plot growth on second age.
-
-plot(dat_out.age_1, dat_out.volume_d, seriestype=:scatter)
-plot!(dat_out.age_1, dat_out.volume_d_hat, seriestype=:scatter)
+plot(dat_out.Age, dat_out.Volume, seriestype=:scatter)
+plot!(dat_out.Age, dat_out.Volume_Hat, seriestype=:scatter)
