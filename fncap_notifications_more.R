c5d2d5683e63af5f7da8a501d8bf27698a47b958
Andrew Steinkruger <amsteinkruger@gmail.com>
2025-12-10 10:53:50 -0800

    Quick update for a histogram.

--- fncap_notifications_more.R
+++ fncap_notifications_more.R
@@ -368,6 +368,7 @@ dat_join_treemap_siteclcd =
   left_join(dat_join_treemap_siteclcd_med)
 
 # TCC
+#  9 minutes as of 2025/12/10.
 
 #  Set up notifications to support comparisons between pre- and post-years.
 
@@ -382,6 +383,8 @@ dat_notifications_tcc =
                names_prefix = "Year_",
                names_to = "Period",
                values_to = "Year")
+
+#  Extract TCC to notifications, then subset results for comparisons of interest.
 
 dat_join_tcc = 
   "output/TCC.tif" %>% 
@@ -408,6 +411,38 @@ dat_join_tcc =
   mutate(TCC_Change = TCC - lag(TCC)) %>% 
   filter(Period == "After") %>% 
   select(UID, TCC_Change)
+
+#  Check the distribution of pre-to-post-notification changes in TCC.
+
+# library(ggpubr)
+# library(RColorBrewer)
+# 
+# pal_tcc_lower = brewer.pal('Reds', n = 8) %>% rev
+# pal_tcc_upper = brewer.pal('Greys', n = 7)
+# pal_tcc = c(pal_tcc_lower, pal_tcc_upper)
+
+# vis_tcc = 
+#   dat_join_tcc %>% 
+#   mutate(bin = cut_interval(TCC_Change, length = 10)) %>% 
+#   group_by(bin) %>% 
+#   summarize(count = n()) %>% 
+#   ungroup %>% 
+#   ggplot() +
+#   geom_col(aes(x = bin,
+#                y = count,
+#                fill = bin),
+#            color = "#000000") +
+#   labs(x = "Change in TCC (Pre- to Post-Notification)",
+#        y = "Notification Count") +
+#   scale_fill_manual(values = pal_tcc) +
+#   scale_y_continuous(expand = c(0, 0)) +
+#   theme_pubr() +
+#   theme(legend.position = "none",
+#         axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1))
+#   
+# ggsave("output/vis_tcc_20251210.png",
+#        dpi = 300,
+#        width = 6.5)
 
 # Landsat
   
