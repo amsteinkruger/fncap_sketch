988c95a1fc8522d9d18ce37bb7443f36d29334c6
Andrew Steinkruger <amsteinkruger@gmail.com>
2026-06-07 22:06:35 -0700

    Iteration for draft.

--- 01_code/3_3_606_Mod.R
+++ 01_code/3_3_606_Mod.R
@@ -22,6 +22,26 @@ dat_explicit =
   mutate(MBF_Bin = ifelse(MBF_Both > 0, 1, 0)) %>% 
   relocate(MBF_Bin, MBF_Both, .after = "Count")
 
+dat_small_explicit = 
+  dat_explicit %>% 
+  group_by(Landowner) %>% 
+  mutate(MBF_All = sum(MBF_Both)) %>% 
+  ungroup %>% 
+  filter(MBF_All <= quantile(MBF_All, 0.50))
+
+dat_large_explicit = 
+  dat_explicit %>% 
+  group_by(Landowner) %>% 
+  mutate(MBF_All = sum(MBF_Both)) %>% 
+  ungroup %>% 
+  filter(MBF_All > quantile(MBF_All, 0.50))
+
+vec_small = dat_small_explicit$Landowner %>% unique
+vec_large = dat_large_explicit$Landowner %>% unique
+
+dat_small = dat %>% filter(Landowner %in% vec_small)
+dat_large = dat %>% filter(Landowner %in% vec_large)
+  
 # Do:
 
 # (1) Hurdle, No Inventory
@@ -73,20 +93,81 @@ mod_hurdle_first_1 =
       SiteClassMode +
       Elevation + 
       Distance_Mill +
-      # MBF_Standing_Forward +
+      MBF_Standing_Forward +
       Stumpage_Lag_.[c(1, 5, 9)] +
       Stumpage_Mean_20 +
       Rate_Lag_.[c(1, 5, 9)] +
       Rate_Mean_20 +
       VPD_Lag_.[c(1, 5, 9)] +
       VPD_Mean_20 +
-      Fire_30_Doughnut_Lag_1 # +
-    # Fire30Doughnut_Mean_20
+      Fire_30_Doughnut_Lag_1
+    | Year,
+    cluster = ~ Landowner,
+    family = binomial(link = "logit"),
+    data = dat_explicit
+  )
+
+mod_hurdle_first_2 = 
+  feglm(
+    MBF_Bin ~
+      SiteClassMode +
+      Elevation + 
+      Distance_Mill +
+      MBF_Standing_Forward +
+      Stumpage_Lag_.[c(1, 5, 9)] +
+      Stumpage_Mean_20 +
+      Rate_Lag_.[c(1, 5, 9)] +
+      Rate_Mean_20 +
+      VPD_Lag_.[c(1, 5, 9)] +
+      VPD_Mean_20 +
+      Fire_30_Doughnut_Lag_1
     | Landowner + Year,
     cluster = ~ Landowner,
     family = binomial(link = "logit"),
     data = dat_explicit
   )
+
+mod_hurdle_first_3 = 
+  feglm(
+    MBF_Bin ~
+      SiteClassMode +
+      Elevation + 
+      Distance_Mill +
+      MBF_Standing_Forward +
+      Stumpage_Lag_.[c(1, 5, 9)] +
+      Stumpage_Mean_20 +
+      Rate_Lag_.[c(1, 5, 9)] +
+      Rate_Mean_20 +
+      VPD_Lag_.[c(1, 5, 9)] +
+      VPD_Mean_20 +
+      Fire_30_Doughnut_Lag_1
+    | Landowner + Year,
+    cluster = ~ Landowner,
+    family = binomial(link = "logit"),
+    data = dat_small_explicit
+  )
+
+mod_hurdle_first_4 = 
+  feglm(
+    MBF_Bin ~
+      SiteClassMode +
+      Elevation + 
+      Distance_Mill +
+      MBF_Standing_Forward +
+      Stumpage_Lag_.[c(1, 5, 9)] +
+      Stumpage_Mean_20 +
+      Rate_Lag_.[c(1, 5, 9)] +
+      Rate_Mean_20 +
+      VPD_Lag_.[c(1, 5, 9)] +
+      VPD_Mean_20 +
+      Fire_30_Doughnut_Lag_1
+    | Landowner + Year,
+    cluster = ~ Landowner,
+    family = binomial(link = "logit"),
+    data = dat_large_explicit
+  )
+
+etable(mod_hurdle_first_1, mod_hurdle_first_2, mod_hurdle_first_3, mod_hurdle_first_4)
 
 #  Second Stage
 
@@ -96,22 +177,103 @@ mod_hurdle_second_1 =
       SiteClassMode +
       Elevation +
       Distance_Mill +
-      # MBF_Standing_Forward +
+      MBF_Standing_Forward +
       Stumpage_Lag_.[c(1, 5, 9)] +
       Stumpage_Mean_20 +
       Rate_Lag_.[c(1, 5, 9)] +
       Rate_Mean_20 +
       VPD_Lag_.[c(1, 5, 9)] +
       VPD_Mean_20 +
-      Fire_30_Doughnut_Lag_1 # +
-    # Fire30Doughnut_Mean_20
+      Fire_30_Doughnut_Lag_1
+    | Year,
+    cluster = ~ Landowner,
+    data = dat)
+
+mod_hurdle_second_2 = 
+  feols(
+    log(MBF_Both) ~ 
+      SiteClassMode +
+      Elevation +
+      Distance_Mill +
+      MBF_Standing_Forward +
+      Stumpage_Lag_.[c(1, 5, 9)] +
+      Stumpage_Mean_20 +
+      Rate_Lag_.[c(1, 5, 9)] +
+      Rate_Mean_20 +
+      VPD_Lag_.[c(1, 5, 9)] +
+      VPD_Mean_20 +
+      Fire_30_Doughnut_Lag_1
     | Landowner + Year,
     cluster = ~ Landowner,
     data = dat)
 
+mod_hurdle_second_3 = 
+  feols(
+    log(MBF_Both) ~ 
+      SiteClassMode +
+      Elevation +
+      Distance_Mill +
+      MBF_Standing_Forward +
+      Stumpage_Lag_.[c(1, 5, 9)] +
+      Stumpage_Mean_20 +
+      Rate_Lag_.[c(1, 5, 9)] +
+      Rate_Mean_20 +
+      VPD_Lag_.[c(1, 5, 9)] +
+      VPD_Mean_20 +
+      Fire_30_Doughnut_Lag_1
+    | Landowner + Year,
+    cluster = ~ Landowner,
+    data = dat_small)
+
+mod_hurdle_second_4 = 
+  feols(
+    log(MBF_Both) ~ 
+      SiteClassMode +
+      Elevation +
+      Distance_Mill +
+      MBF_Standing_Forward +
+      Stumpage_Lag_.[c(1, 5, 9)] +
+      Stumpage_Mean_20 +
+      Rate_Lag_.[c(1, 5, 9)] +
+      Rate_Mean_20 +
+      VPD_Lag_.[c(1, 5, 9)] +
+      VPD_Mean_20 +
+      Fire_30_Doughnut_Lag_1
+    | Landowner + Year,
+    cluster = ~ Landowner,
+    data = dat_large)
+
+
+etable(mod_hurdle_second_1, mod_hurdle_second_2, mod_hurdle_second_3, mod_hurdle_second_4)
+
 #  AME
 
 #  SE
+
+# Exports
+
+library(modelsummary)
+library(flextable)
+
+modelsummary(
+  list("A" = mod_hurdle_first_1,
+       "B" = mod_hurdle_first_2,
+       "C" = mod_hurdle_first_3,
+       "D" = mod_hurdle_first_4),
+       stars = TRUE, 
+       output = "flextable") |> 
+  autofit() |> 
+  save_as_docx(path = "04_out/tab_first.docx")
+
+modelsummary(
+  list("A" = mod_hurdle_second_1,
+       "B" = mod_hurdle_second_2,
+       "C" = mod_hurdle_second_3,
+       "D" = mod_hurdle_second_4),
+  stars = TRUE, 
+  output = "flextable") |> 
+  autofit() |> 
+  save_as_docx(path = "04_out/tab_second.docx")
 
 # Checking out mhurdle: appears to throw computational errors pretty often.
 
@@ -143,7 +305,7 @@ mod_hurdle_second_1 =
 #        style.tex = style.tex("aer"),
 #        file = "04_out/tab_test.tex")
 
-write_csv(mod_hurdle_first_1 %>% tidy, "04_out/tab_mod_first.csv")
-write_csv(mod_hurdle_second_1 %>% tidy, "04_out/tab_mod_second.csv")
+# write_csv(mod_hurdle_first_1 %>% tidy, "04_out/tab_mod_first.csv")
+# write_csv(mod_hurdle_second_1 %>% tidy, "04_out/tab_mod_second.csv")
 
 # write_csv(dat_ame, "04_out/tab_ame.csv")
--- 01_code/3_4_606_Vis_1.R
+++ 01_code/3_4_606_Vis_1.R
@@ -1,182 +1,112 @@
-# Visualize something for 2YP.
+# Visualize the study region.
+
+#  Palette
+
+library(RColorBrewer)
+
+pal_fire = brewer.pal(9, "Reds")[c(4, 6, 8)] %>% rev
+pal_timber = brewer.pal(9, "Greens")[8]
+pal_both = c(pal_fire, pal_timber)
+
+#  Data
+
+#   Region
 
 dat_bounds = 
   "03_intermediate/dat_bounds.gdb" %>% 
   vect
 
-dat = 
-  "03_intermediate/dat_notifications_1_8.gdb" %>% 
+#   States
+
+dat_states = 
+  "02_data/1_2_1_Census_States" %>% 
   vect %>% 
-  mutate(Year = DateStart %>% year,
-         Month = DateStart %>% month,
-         Quarter = Month %>% multiply_by(1 / 3) %>% ceiling,
-         Year_Quarter = paste0(Year, "_Q", Quarter)) %>% 
-  mutate(Restrict_MBF_Lower = MBF_2_DouglasFir > quantile(MBF_2_DouglasFir, 0.01),
-         Restrict_MBF_Upper = MBF_2_DouglasFir < quantile(MBF_2_DouglasFir, 0.99),
-         Restrict_Acres_Lower = Acres_1 > quantile(Acres_1, 0.01),
-         Restrict_Acres_Upper = Acres_1 < quantile(Acres_1, 0.99),
-         Restrict_MBFAcre_Lower = MBF_Acre_2_DouglasFir > quantile(MBF_Acre_2_DouglasFir, 0.01),
-         Restrict_MBFAcre_Upper = MBF_Acre_2_DouglasFir < quantile(MBF_Acre_2_DouglasFir, 0.99)) %>% 
-  filter(if_all(starts_with("Restrict"), ~ .x == TRUE)) %>% 
-  select(Year, MBF_2_DouglasFir, MBF_Acre_2_DouglasFir, Landowner) %>% 
-  mutate(Landowner = Landowner %>% factor %>% as.numeric)
-  
+  # filter(STUSPS == "OR") %>% 
+  # select(STUSPS) %>% 
+  project("EPSG:2992")
 
-# Figure 1: production, yield, firms in space
+#   Pyromes/Ecoregions
 
-# Geodata
+dat_pyromes = 
+  "02_data/1_2_2_USFS_Pyromes/Data/Pyromes_CONUS_20200206.shp" %>% 
+  vect %>% 
+  rename(WHICH = NAME) %>% # Band-Aid for a reserved attribute name.
+  filter(WHICH %in% c("Marine Northwest Coast Forest", "Klamath Mountains", "Middle Cascades")) %>% 
+  select(Pyrome = WHICH) %>% 
+  project("EPSG:2992") %>% 
+  crop(dat_bounds)
 
-#  Raster
+#   Places
 
-dat_rast = dat %>% rast(ncol = 50, nrow = 100)
+#   Private Forest/Timberland
 
-#  Allocate lists for each metric. Remember that iteration can be messy.
+dat_owner = 
+  "02_data/3_4_ODF_Ownership/Ownership.gdb" %>% 
+  vect %>% 
+  select(Owner = LandManager) %>% 
+  project("EPSG:2992") %>% 
+  crop(dat_bounds) %>% 
+  group_by(Owner) %>% 
+  summarize %>% 
+  ungroup %>% 
+  mutate(Fill = "Private\nTimberland")
 
-dat_list_production <- list()
-dat_list_yield <- list()
-dat_list_companies <- list()
+#   Fires
 
-# Production
+dat_mtbs = 
+  "02_data/1_7_1_MTBS/Perimeters" %>% 
+  vect %>% 
+  project("EPSG:2992") %>% 
+  makeValid %>% 
+  crop(dat_bounds) %>% 
+  mutate(Year_MTBS = ig_date %>% year, 
+         .keep = "none") %>% 
+  mutate(Fill = 
+           case_when(Year_MTBS %in% 1984:1999 ~ "Wildfire,\n1984-1999",
+                     Year_MTBS %in% 2000:2019 ~ "Wildfire,\n2000-2019",
+                     Year_MTBS %in% 2020:2025 ~ "Wildfire,\n2020-2025") %>% 
+           factor %>% 
+           fct_rev)
 
-for (i in 1:nrow(dat)) {
-  # Extract polygon i. 
-  polygon <- dat[i, ]
-  
-  # Assign 1 to cells in polygons.
-  r <- rasterize(polygon, dat_rast, field = "MBF_2_DouglasFir", background = NA, touches = TRUE)
-  
-  # Name layers.
-  names(r) <- paste0("polygon_", i)
-  
-  # Iterate into a pre-allocated list. 
-  dat_list_production[[i]] <- r
-}
+#    Intersection
 
-dat_raster_production <- rast(dat_list_production)
+# dat_both = intersect(dat_owner, dat_mtbs)
 
-dat_raster_production_sum <- app(dat_raster_production, fun = sum, na.rm = TRUE)
+#  Visualizations
 
-dat_raster_production_sum_mean = dat_raster_production_sum / 40
+#   Main Plot
 
-# Yield
-
-for (i in 1:nrow(dat)) {
-  # Extract polygon i. 
-  polygon <- dat[i, ]
-  
-  # Assign 1 to cells in polygons.
-  r <- rasterize(polygon, dat_rast, field = "MBF_Acre_2_DouglasFir", background = NA, touches = TRUE)
-  
-  # Name layers.
-  names(r) <- paste0("polygon_", i)
-  
-  # Iterate into a pre-allocated list. 
-  dat_list_yield[[i]] <- r
-}
-
-dat_raster_yield <- rast(dat_list_yield)
-
-dat_raster_yield_mean <- app(dat_raster_yield, fun = mean, na.rm = TRUE)
-
-# Company Counts
-
-for (i in 1:nrow(dat)) {
-  # Extract polygon i. 
-  polygon <- dat[i, ]
-  
-  # Assign 1 to cells in polygons.
-  r <- rasterize(polygon, dat_rast, field = "Landowner", background = NA, touches = TRUE)
-  
-  # Name layers.
-  names(r) <- paste0("polygon_", i)
-  
-  # Iterate into a pre-allocated list. 
-  dat_list_companies[[i]] <- r
-}
-
-dat_raster_companies <- rast(dat_list_companies)
-
-dat_raster_companies_count <- app(dat_raster_companies, fun = function(x){length(unique(x))}) # function(x) length(unique(as.character(x), na.rm = TRUE))
-
-dat_raster_companies_count = dat_raster_companies_count - 1
-dat_raster_companies_count = dat_raster_companies_count %>% subst(0, NA)
-
-# Plots
-
-vis_production = 
-  ggplot() +
-  geom_spatraster(data = dat_raster_production_sum_mean / 1000) +
-  geom_spatvector(data = dat_bounds, fill = "NA", color = "black") +
-  scale_fill_distiller(palette = "Greens",
-                       direction = 1,
-                       limits = c(0, 1),
-                       breaks = c(0, 1),
-                       guide = guide_colorbar(title.position = "top"),
-                       na.value = NA) +
-  # scale_fill_viridis(option = "D", 
-  #                    limits = c(0, NA),
-  #                    # breaks = c(0, 5),
-  #                    guide = guide_colorbar(title.position = "top"),
-  #                    na.value = NA) +
-  labs(fill = "Mean Annual MMBF") +
+vis_main = 
+  ggplot() + 
+  geom_spatvector(data = dat_bounds,
+                  fill = NA, 
+                  color = "black") + 
+  geom_spatvector(data = dat_owner, 
+                  aes(fill = Fill),
+                  color = NA, 
+                  alpha = 1.00) +
+  geom_spatvector(data = dat_mtbs, 
+                  aes(fill = Fill),
+                  color = NA, 
+                  alpha = 0.75) +
+  scale_fill_manual(values = pal_both) +
   theme_void() +
-  theme(legend.position = "bottom",
-        legend.direction = "horizontal",
-        legend.ticks = element_blank(),
-        legend.key.height = unit(0.25, "lines"),
-        legend.key.width = unit(1.5, "lines"),
-        legend.key = element_rect(fill = NA, color = "black"),
-        legend.text = element_text(size = 8),
-        legend.title = element_text(size = 9, hjust = 0.5))
+  theme(legend.position = "inside",
+        legend.position.inside = c(-0.05, 0.50),
+        # legend.direction = "vertical",
+        legend.key.height = unit(5.00, "lines"),
+        legend.key.width = unit(0.50, "lines"),
+        # legend.key = element_rect(fill = NA, color = "black"),
+        # legend.text = element_text(size = 8),
+        legend.text = 
+          element_text(angle = 90,
+                       hjust = 0.00,
+                       margin = margin(t = 0, r = 0, b = 0.50, l = 0.50, unit = "lines")),
+        legend.text.position = "left",
+        legend.title = element_blank())
 
-vis_yield = 
-  ggplot() +
-  geom_spatvector(data = dat_bounds, fill = "white", color = "black") +
-  geom_spatraster(data = dat_raster_yield_mean) +
-  scale_fill_distiller(palette = "Blues",
-                       direction = 1,
-                       limits = c(0, 60),
-                       breaks = c(0, 60),
-                       guide = guide_colorbar(title.position = "top"),
-                       na.value = NA) +
-  labs(fill = "Mean Yield (MBF/Acre)") +
-  theme_void() +
-  theme(legend.position = "bottom",
-        legend.direction = "horizontal",
-        legend.ticks = element_blank(),
-        legend.key.height = unit(0.25, "lines"),
-        legend.key.width = unit(1.5, "lines"),
-        legend.key = element_rect(fill = NA, color = "black"),
-        legend.text = element_text(size = 8),
-        legend.title = element_text(size = 9, hjust = 0.5))
-
-vis_companies = 
-  ggplot() +
-  geom_spatvector(data = dat_bounds, fill = "white", color = "black") +
-  geom_spatraster(data = dat_raster_companies_count) +
-  scale_fill_distiller(palette = "Oranges",
-                       direction = 1,
-                       limits = c(0, 10),
-                       breaks = c(0, 10),
-                       guide = guide_colorbar(title.position = "top"),
-                       na.value = NA) +
-  labs(fill = "Total Active Firms") +
-  theme_void() +
-  theme(legend.position = "bottom",
-        legend.direction = "horizontal",
-        legend.ticks = element_blank(),
-        legend.key.height = unit(0.25, "lines"),
-        legend.key.width = unit(1.5, "lines"),
-        legend.key = element_rect(fill = NA, color = "black"),
-        legend.text = element_text(size = 8),
-        legend.title = element_text(size = 9, hjust = 0.5))
-
-# Combine/Export
-
-vis_1 = vis_production + vis_yield + vis_companies
-
-ggsave("04_out/vis_1_20260428.png",
-       vis_1,
+ggsave("04_out/Presentation/vis_1_map.png",
+       vis_main,
        dpi = 300,
-       width = 6.5,
-       height = 4.5)
+       height = 4.5,
+       width = 6.5)
