e618b676c5f326f7e0b8253056d13464f841d2f3
Andrew Steinkruger <amsteinkruger@gmail.com>
2026-08-04 09:34:39 -0700

    More estimation.

--- 01_code/0_x_process_growth.R
+++ 01_code/0_x_process_growth.R
@@ -108,7 +108,7 @@ dat_use =
   # Cut western hemlock for now. 
   filter(SPCD == 202) %>%
   # Cut stands older than 75 years for now.
-  filter(STDAGE <= 75) %>%
+  filter(STDAGE <= 100) %>%
   # Handle outliers.
   filter(ntile(VOLBFNET_ACRE, 100) %in% 2:99) %>% 
   filter(ntile(VOLBFNET_ACRE / STDAGE, 100) %in% 2:99) %>% 
@@ -116,9 +116,8 @@ dat_use =
   filter(ntile(ANN_NET_GROWTH_ACRE, 100) %in% 2:99 | is.na(ANN_NET_GROWTH_ACRE)) %>% 
   filter(ntile(ANN_NET_GROWTH_ACRE / VOLBFNET_ACRE, 100) %in% 2:99 | is.na(ANN_NET_GROWTH_ACRE))
 
-# (before species, age drops)
-# 2970 year-plot-condition-species, 2306 Douglas fir, 664 western hemlock with volume or growth.
-# 794, 620, 174 with growth
+# 2262 observations for Douglas fir without age restriction.
+# 2192 observations for Douglas fir with STDAGE < 100. 
 
 plot(dat_use$STDAGE, dat_use$VOLBFNET_ACRE)
 plot(dat_use$EST_BEGIN_ACRE, dat_use$ANN_NET_GROWTH_ACRE)
@@ -136,6 +135,11 @@ vis_1 =
 
 # Estimation
 
+#  SITECLCD Bins
+
+vec_fast = 1:3
+vec_slow = 4:7
+
 #  Yield ~ Age
 
 #   Linear
@@ -147,15 +151,21 @@ mod_yield_fir_linear =
 par_yield_fir_linear_a = mod_yield_fir_linear$coefficients[[1]]
 par_yield_fir_linear_b = mod_yield_fir_linear$coefficients[[2]]
 
-#   Exponential
-
-mod_yield_fir_exponential = 
+mod_yield_fir_linear_fast = 
   dat_use %>% 
-  mutate(VOLBFNET_ACRE_LOG = VOLBFNET_ACRE %>% log) %>% 
-  lm(VOLBFNET_ACRE_LOG ~ STDAGE, data = .)
+  filter(SITECLCD %in% vec_fast) %>% 
+  lm(VOLBFNET_ACRE ~ STDAGE, data = .)
 
-par_yield_fir_exponential_a = mod_yield_fir_exponential$coefficients[[1]] %>% exp
-par_yield_fir_exponential_b = mod_yield_fir_exponential$coefficients[[2]]
+par_yield_fir_linear_fast_a = mod_yield_fir_linear_fast$coefficients[[1]]
+par_yield_fir_linear_fast_b = mod_yield_fir_linear_fast$coefficients[[2]]
+
+mod_yield_fir_linear_slow = 
+  dat_use %>% 
+  filter(SITECLCD %in% vec_slow) %>% 
+  lm(VOLBFNET_ACRE ~ STDAGE, data = .)
+
+par_yield_fir_linear_slow_a = mod_yield_fir_linear_slow$coefficients[[1]]
+par_yield_fir_linear_slow_b = mod_yield_fir_linear_slow$coefficients[[2]]
 
 #   Logistic
 
@@ -171,6 +181,32 @@ par_yield_fir_logistic_k = mod_yield_fir_logistic %>% coef %>% magrittr::extract
 par_yield_fir_logistic_a = mod_yield_fir_logistic %>% coef %>% magrittr::extract(2)
 par_yield_fir_logistic_b = mod_yield_fir_logistic %>% coef %>% magrittr::extract(3)
 
+mod_yield_fir_logistic_fast = 
+  dat_use %>% 
+  filter(SITECLCD %in% vec_fast) %>% 
+  nls(
+    VOLBFNET_ACRE ~ k / (1 + a * exp(-b * STDAGE)),
+    data = .,
+    start = list(k = 60000, a = 50, b = 0.10)
+  )
+
+par_yield_fir_logistic_fast_k = mod_yield_fir_logistic_fast %>% coef %>% magrittr::extract(1)
+par_yield_fir_logistic_fast_a = mod_yield_fir_logistic_fast %>% coef %>% magrittr::extract(2)
+par_yield_fir_logistic_fast_b = mod_yield_fir_logistic_fast %>% coef %>% magrittr::extract(3)
+
+mod_yield_fir_logistic_slow = 
+  dat_use %>% 
+  filter(SITECLCD %in% vec_slow) %>% 
+  nls(
+    VOLBFNET_ACRE ~ k / (1 + a * exp(-b * STDAGE)),
+    data = .,
+    start = list(k = 60000, a = 50, b = 0.10)
+  )
+
+par_yield_fir_logistic_slow_k = mod_yield_fir_logistic_slow %>% coef %>% magrittr::extract(1)
+par_yield_fir_logistic_slow_a = mod_yield_fir_logistic_slow %>% coef %>% magrittr::extract(2)
+par_yield_fir_logistic_slow_b = mod_yield_fir_logistic_slow %>% coef %>% magrittr::extract(3)
+
 #   Chang (1984), Hashida and Fenichel (2021)
 
 mod_yield_fir_chang = 
@@ -183,6 +219,30 @@ mod_yield_fir_chang =
 par_yield_fir_chang_0 = mod_yield_fir_chang$coefficients[[1]]
 par_yield_fir_chang_1 = mod_yield_fir_chang$coefficients[[2]]
 par_yield_fir_chang_2 = mod_yield_fir_chang$coefficients[[3]]
+
+mod_yield_fir_chang_fast = 
+  dat_use %>% 
+  filter(SITECLCD %in% vec_fast) %>% 
+  mutate(STDAGE_INVERSE = STDAGE ^ -1,
+         STDAGE_INVERSE_SQUARE = STDAGE ^ -2,
+         VOLBFNET_ACRE_LOG = VOLBFNET_ACRE %>% log) %>% 
+  lm(VOLBFNET_ACRE_LOG ~ STDAGE_INVERSE + STDAGE_INVERSE_SQUARE, data = .)
+
+par_yield_fir_chang_fast_0 = mod_yield_fir_chang_fast$coefficients[[1]]
+par_yield_fir_chang_fast_1 = mod_yield_fir_chang_fast$coefficients[[2]]
+par_yield_fir_chang_fast_2 = mod_yield_fir_chang_fast$coefficients[[3]]
+
+mod_yield_fir_chang_slow = 
+  dat_use %>% 
+  filter(SITECLCD %in% vec_slow) %>% 
+  mutate(STDAGE_INVERSE = STDAGE ^ -1,
+         STDAGE_INVERSE_SQUARE = STDAGE ^ -2,
+         VOLBFNET_ACRE_LOG = VOLBFNET_ACRE %>% log) %>% 
+  lm(VOLBFNET_ACRE_LOG ~ STDAGE_INVERSE + STDAGE_INVERSE_SQUARE, data = .)
+
+par_yield_fir_chang_slow_0 = mod_yield_fir_chang_slow$coefficients[[1]]
+par_yield_fir_chang_slow_1 = mod_yield_fir_chang_slow$coefficients[[2]]
+par_yield_fir_chang_slow_2 = mod_yield_fir_chang_slow$coefficients[[3]]
 
 #   Chapman-Richards
 #    Note that without reference to MCC or MAI via FIA, this does not meaningfully follow Chisholm and Gray. 
@@ -200,6 +260,34 @@ par_yield_fir_chapmanrichards_a = mod_yield_fir_chapmanrichards %>% coef %>% mag
 par_yield_fir_chapmanrichards_p = mod_yield_fir_chapmanrichards %>% coef %>% magrittr::extract(2)
 par_yield_fir_chapmanrichards_k = mod_yield_fir_chapmanrichards %>% coef %>% magrittr::extract(3)
 
+mod_yield_fir_chapmanrichards_fast = 
+  dat_use %>% 
+  filter(SITECLCD %in% vec_fast) %>% 
+  mutate(VOLBFNET_ACRE_LOG = VOLBFNET_ACRE %>% log) %>% 
+  nls(
+    VOLBFNET_ACRE_LOG ~ a + p * log(1 - exp(-k * STDAGE)),
+    data = .,
+    start = list(a = 60000, p = 100, k = 0.01)
+  )
+
+par_yield_fir_chapmanrichards_fast_a = mod_yield_fir_chapmanrichards_fast %>% coef %>% magrittr::extract(1)
+par_yield_fir_chapmanrichards_fast_p = mod_yield_fir_chapmanrichards_fast %>% coef %>% magrittr::extract(2)
+par_yield_fir_chapmanrichards_fast_k = mod_yield_fir_chapmanrichards_fast %>% coef %>% magrittr::extract(3)
+
+mod_yield_fir_chapmanrichards_slow = 
+  dat_use %>% 
+  filter(SITECLCD %in% vec_slow) %>% 
+  mutate(VOLBFNET_ACRE_LOG = VOLBFNET_ACRE %>% log) %>% 
+  nls(
+    VOLBFNET_ACRE_LOG ~ a + p * log(1 - exp(-k * STDAGE)),
+    data = .,
+    start = list(a = 60000, p = 100, k = 0.01)
+  )
+
+par_yield_fir_chapmanrichards_slow_a = mod_yield_fir_chapmanrichards_slow %>% coef %>% magrittr::extract(1)
+par_yield_fir_chapmanrichards_slow_p = mod_yield_fir_chapmanrichards_slow %>% coef %>% magrittr::extract(2)
+par_yield_fir_chapmanrichards_slow_k = mod_yield_fir_chapmanrichards_slow %>% coef %>% magrittr::extract(3)
+
 #  Yield_1 ~ Yield_0
 
 #   Linear
@@ -211,15 +299,21 @@ mod_growth_fir_linear =
 par_growth_fir_linear_a = mod_growth_fir_linear$coefficients[[1]]
 par_growth_fir_linear_b = mod_growth_fir_linear$coefficients[[2]]
 
-#   Exponential
-
-mod_growth_fir_exponential = 
+mod_growth_fir_linear_fast = 
   dat_use %>% 
-  mutate(ANN_NET_GROWTH_ACRE_LOG = ANN_NET_GROWTH_ACRE %>% log) %>% 
-  lm(ANN_NET_GROWTH_ACRE_LOG ~ EST_BEGIN_ACRE, data = .)
+  filter(SITECLCD %in% vec_fast) %>% 
+  lm(ANN_NET_GROWTH_ACRE ~ EST_BEGIN_ACRE, data = .)
 
-par_growth_fir_exponential_a = mod_growth_fir_exponential$coefficients[[1]] %>% exp
-par_growth_fir_exponential_b = mod_growth_fir_exponential$coefficients[[2]]
+par_growth_fir_linear_fast_a = mod_growth_fir_linear_fast$coefficients[[1]]
+par_growth_fir_linear_fast_b = mod_growth_fir_linear_fast$coefficients[[2]]
+
+mod_growth_fir_linear_slow = 
+  dat_use %>% 
+  filter(SITECLCD %in% vec_slow) %>% 
+  lm(ANN_NET_GROWTH_ACRE ~ EST_BEGIN_ACRE, data = .)
+
+par_growth_fir_linear_slow_a = mod_growth_fir_linear_slow$coefficients[[1]]
+par_growth_fir_linear_slow_b = mod_growth_fir_linear_slow$coefficients[[2]]
 
 #   Logistic
 
@@ -234,6 +328,30 @@ mod_growth_fir_logistic =
 par_growth_fir_logistic_r = mod_growth_fir_logistic %>% coef %>% magrittr::extract(1)
 par_growth_fir_logistic_k = mod_growth_fir_logistic %>% coef %>% magrittr::extract(2)
 
+mod_growth_fir_logistic_fast =
+  dat_use %>% 
+  filter(SITECLCD %in% vec_fast) %>% 
+  nls(
+    ANN_NET_GROWTH_ACRE ~ r * EST_BEGIN_ACRE * (1 - EST_BEGIN_ACRE / k),
+    data = .,
+    start = list(r = 1.10, k = 60000)
+  )
+
+par_growth_fir_logistic_fast_r = mod_growth_fir_logistic_fast %>% coef %>% magrittr::extract(1)
+par_growth_fir_logistic_fast_k = mod_growth_fir_logistic_fast %>% coef %>% magrittr::extract(2)
+
+mod_growth_fir_logistic_slow =
+  dat_use %>% 
+  filter(SITECLCD %in% vec_slow) %>% 
+  nls(
+    ANN_NET_GROWTH_ACRE ~ r * EST_BEGIN_ACRE * (1 - EST_BEGIN_ACRE / k),
+    data = .,
+    start = list(r = 1.10, k = 60000)
+  )
+
+par_growth_fir_logistic_slow_r = mod_growth_fir_logistic_slow %>% coef %>% magrittr::extract(1)
+par_growth_fir_logistic_slow_k = mod_growth_fir_logistic_slow %>% coef %>% magrittr::extract(2)
+
 #   Ricker
 
 mod_growth_fir_ricker = 
@@ -242,8 +360,28 @@ mod_growth_fir_ricker =
          MBF_INITIAL = EST_BEGIN_ACRE) %>% 
   lm(MBF_QUOTIENT_LOG ~ MBF_INITIAL, data = .)
 
-par_growth_fir_ricker_r = mod_fir_ricker$coefficients[[1]]
-par_growth_fir_ricker_k = - mod_fir_ricker$coefficients[[1]] / mod_fir_ricker$coefficients[[2]]
+par_growth_fir_ricker_r = mod_growth_fir_ricker$coefficients[[1]]
+par_growth_fir_ricker_k = - mod_growth_fir_ricker$coefficients[[1]] / mod_growth_fir_ricker$coefficients[[2]]
+
+mod_growth_fir_ricker_fast = 
+  dat_use %>% 
+  filter(SITECLCD %in% vec_fast) %>% 
+  mutate(MBF_QUOTIENT_LOG = log(ANN_NET_GROWTH_ACRE / EST_BEGIN_ACRE),
+         MBF_INITIAL = EST_BEGIN_ACRE) %>% 
+  lm(MBF_QUOTIENT_LOG ~ MBF_INITIAL, data = .)
+
+par_growth_fir_ricker_fast_r = mod_growth_fir_ricker_fast$coefficients[[1]]
+par_growth_fir_ricker_fast_k = - mod_growth_fir_ricker_fast$coefficients[[1]] / mod_growth_fir_ricker_fast$coefficients[[2]]
+
+mod_growth_fir_ricker_slow = 
+  dat_use %>% 
+  filter(SITECLCD %in% vec_slow) %>% 
+  mutate(MBF_QUOTIENT_LOG = log(ANN_NET_GROWTH_ACRE / EST_BEGIN_ACRE),
+         MBF_INITIAL = EST_BEGIN_ACRE) %>% 
+  lm(MBF_QUOTIENT_LOG ~ MBF_INITIAL, data = .)
+
+par_growth_fir_ricker_slow_r = mod_growth_fir_ricker_slow$coefficients[[1]]
+par_growth_fir_ricker_slow_k = - mod_growth_fir_ricker_slow$coefficients[[1]] / mod_growth_fir_ricker_slow$coefficients[[2]]
 
 #  Beverton-Holt
 
@@ -255,38 +393,92 @@ mod_growth_fir_bevertonholt =
     start = list(r = 1.10, k = 60000)
     )
 
-par_growth_fir_bevertonholt_r = mod_fir_bevertonholt %>% coef %>% magrittr::extract(1)
-par_growth_fir_bevertonholt_k = mod_fir_bevertonholt %>% coef %>% magrittr::extract(2)
+par_growth_fir_bevertonholt_r = mod_growth_fir_bevertonholt %>% coef %>% magrittr::extract(1)
+par_growth_fir_bevertonholt_k = mod_growth_fir_bevertonholt %>% coef %>% magrittr::extract(2)
 
-#  Varying: site class, species handling?, ecoregion, county
+mod_growth_fir_bevertonholt_fast =
+  dat_use %>% 
+  filter(SITECLCD %in% vec_fast) %>% 
+  nls(
+    ANN_NET_GROWTH_ACRE ~ EST_BEGIN_ACRE * (r / (1 + ((r - 1) / k) * EST_BEGIN_ACRE)),
+    data = .,
+    start = list(r = 1.10, k = 60000)
+  )
+
+par_growth_fir_bevertonholt_fast_r = mod_growth_fir_bevertonholt_fast %>% coef %>% magrittr::extract(1)
+par_growth_fir_bevertonholt_fast_k = mod_growth_fir_bevertonholt_fast %>% coef %>% magrittr::extract(2)
+
+mod_growth_fir_bevertonholt_slow =
+  dat_use %>% 
+  filter(SITECLCD %in% vec_slow) %>% 
+  nls(
+    ANN_NET_GROWTH_ACRE ~ EST_BEGIN_ACRE * (r / (1 + ((r - 1) / k) * EST_BEGIN_ACRE)),
+    data = .,
+    start = list(r = 1.10, k = 60000)
+  )
+
+par_growth_fir_bevertonholt_slow_r = mod_growth_fir_bevertonholt_slow %>% coef %>% magrittr::extract(1)
+par_growth_fir_bevertonholt_slow_k = mod_growth_fir_bevertonholt_slow %>% coef %>% magrittr::extract(2)
+
+#  Vary: site class, species, ecoregion, county
 
 # Estimation w/ Stochastic Component
 
 
 # Visualization
 
-#  each model with residuals against data separately
-#  then all models together with data faded to minimize visual noise
-
-# switch from points to stat_function
+#  Get point estimates. 
 
 dat_fir_model = 
   dat_use %>% 
   mutate(
     # Yield
     VOLBFNET_ACRE_FIR_LINEAR = par_yield_fir_linear_a + par_yield_fir_linear_b * STDAGE,
-    VOLBFNET_ACRE_FIR_EXPONENTIAL = par_yield_fir_exponential_a * exp(par_yield_fir_exponential_b * STDAGE),
     VOLBFNET_ACRE_FIR_LOGISTIC = par_yield_fir_logistic_k / (1 + par_yield_fir_logistic_a * exp(-par_yield_fir_logistic_b * STDAGE)),
     VOLBFNET_ACRE_FIR_CHANG = exp(par_yield_fir_chang_0) * exp(par_yield_fir_chang_1 * STDAGE ^ -1 + par_yield_fir_chang_2 * STDAGE ^ -2),
     VOLBFNET_ACRE_FIR_CHAPMANRICHARDS = exp(par_yield_fir_chapmanrichards_a) * (1 - exp(- par_yield_fir_chapmanrichards_k * STDAGE)) ^ par_yield_fir_chapmanrichards_p,
     # Growth
     ANN_NET_GROWTH_ACRE_FIR_LINEAR = par_growth_fir_linear_a + par_growth_fir_linear_b * EST_BEGIN_ACRE,
-    ANN_NET_GROWTH_ACRE_FIR_EXPONENTIAL = par_growth_fir_exponential_a * exp(par_growth_fir_exponential_b * EST_BEGIN_ACRE),
     ANN_NET_GROWTH_ACRE_FIR_LOGISTIC = par_growth_fir_logistic_r * EST_BEGIN_ACRE * (1 - EST_BEGIN_ACRE / par_growth_fir_logistic_k),
     ANN_NET_GROWTH_ACRE_FIR_RICKER = EST_BEGIN_ACRE * exp(par_growth_fir_ricker_r * (1 - EST_BEGIN_ACRE / par_growth_fir_ricker_k)),
     ANN_NET_GROWTH_ACRE_FIR_BEVERTONHOLT = EST_BEGIN_ACRE * (par_growth_fir_bevertonholt_r / (1 + ((par_growth_fir_bevertonholt_r - 1) / par_growth_fir_bevertonholt_k) * EST_BEGIN_ACRE)) 
     )
-  
+
+dat_fir_model_fast = 
+  dat_use %>% 
+  filter(SITECLCD %in% vec_fast) %>% 
+  mutate(
+    # Yield
+    VOLBFNET_ACRE_FIR_LINEAR = par_yield_fir_linear_fast_a + par_yield_fir_linear_fast_b * STDAGE,
+    VOLBFNET_ACRE_FIR_LOGISTIC = par_yield_fir_logistic_fast_k / (1 + par_yield_fir_logistic_fast_a * exp(-par_yield_fir_logistic_fast_b * STDAGE)),
+    VOLBFNET_ACRE_FIR_CHANG = exp(par_yield_fir_chang_fast_0) * exp(par_yield_fir_chang_fast_1 * STDAGE ^ -1 + par_yield_fir_chang_fast_2 * STDAGE ^ -2),
+    VOLBFNET_ACRE_FIR_CHAPMANRICHARDS = exp(par_yield_fir_chapmanrichards_fast_a) * (1 - exp(- par_yield_fir_chapmanrichards_fast_k * STDAGE)) ^ par_yield_fir_chapmanrichards_fast_p,
+    # Growth
+    ANN_NET_GROWTH_ACRE_FIR_LINEAR = par_growth_fir_linear_fast_a + par_growth_fir_linear_fast_b * EST_BEGIN_ACRE,
+    ANN_NET_GROWTH_ACRE_FIR_LOGISTIC = par_growth_fir_logistic_fast_r * EST_BEGIN_ACRE * (1 - EST_BEGIN_ACRE / par_growth_fir_logistic_fast_k),
+    ANN_NET_GROWTH_ACRE_FIR_RICKER = EST_BEGIN_ACRE * exp(par_growth_fir_ricker_fast_r * (1 - EST_BEGIN_ACRE / par_growth_fir_ricker_fast_k)),
+    ANN_NET_GROWTH_ACRE_FIR_BEVERTONHOLT = EST_BEGIN_ACRE * (par_growth_fir_bevertonholt_fast_r / (1 + ((par_growth_fir_bevertonholt_fast_r - 1) / par_growth_fir_bevertonholt_fast_k) * EST_BEGIN_ACRE)) 
+  )
+
+dat_fir_model_slow = 
+  dat_use %>% 
+  filter(SITECLCD %!in% vec_slow) %>% 
+  mutate(
+    # Yield
+    VOLBFNET_ACRE_FIR_LINEAR = par_yield_fir_linear_slow_a + par_yield_fir_linear_slow_b * STDAGE,
+    VOLBFNET_ACRE_FIR_LOGISTIC = par_yield_fir_logistic_slow_k / (1 + par_yield_fir_logistic_slow_a * exp(-par_yield_fir_logistic_slow_b * STDAGE)),
+    VOLBFNET_ACRE_FIR_CHANG = exp(par_yield_fir_chang_slow_0) * exp(par_yield_fir_chang_slow_1 * STDAGE ^ -1 + par_yield_fir_chang_slow_2 * STDAGE ^ -2),
+    VOLBFNET_ACRE_FIR_CHAPMANRICHARDS = exp(par_yield_fir_chapmanrichards_slow_a) * (1 - exp(- par_yield_fir_chapmanrichards_slow_k * STDAGE)) ^ par_yield_fir_chapmanrichards_slow_p,
+    # Growth
+    ANN_NET_GROWTH_ACRE_FIR_LINEAR = par_growth_fir_linear_slow_a + par_growth_fir_linear_slow_b * EST_BEGIN_ACRE,
+    ANN_NET_GROWTH_ACRE_FIR_LOGISTIC = par_growth_fir_logistic_slow_r * EST_BEGIN_ACRE * (1 - EST_BEGIN_ACRE / par_growth_fir_logistic_slow_k),
+    ANN_NET_GROWTH_ACRE_FIR_RICKER = EST_BEGIN_ACRE * exp(par_growth_fir_ricker_slow_r * (1 - EST_BEGIN_ACRE / par_growth_fir_ricker_slow_k)),
+    ANN_NET_GROWTH_ACRE_FIR_BEVERTONHOLT = EST_BEGIN_ACRE * (par_growth_fir_bevertonholt_slow_r / (1 + ((par_growth_fir_bevertonholt_slow_r - 1) / par_growth_fir_bevertonholt_slow_k) * EST_BEGIN_ACRE)) 
+  )  
+
+
+
+
 vis_yield_fir_linear = 
   dat_fir_model %>% 
   ggplot() +
