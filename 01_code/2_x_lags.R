1b2545bd4ce163133d337f3ac76a91680b102979
Andrew Steinkruger <amsteinkruger@gmail.com>
2026-09-02 00:11:04 -0700

    Through time series wrangling.

--- 01_code/2_x_lags.R
+++ 01_code/2_x_lags.R
@@ -1,23 +1,10 @@
 # Visualize time series of yield, supply, and time-varying covariates.
 
-#  Try deltas as well as level values. 
-#  Try doing something about seasonality. 
-
 # correlations within raw X
 
 # correlations with lags (corr matrices I guess?)
 
 # correlations between Y, X
-
-# so, column goals:
-# (x) lags 1:40
-# (x) means 1, 4, 8, 12
-# ( ) level values less seasonal means
-# ( ) change values
-# ( ) change values less seasonal means
-
-# maybe need demeaned levels, change, demeaned change before lags, means
-# then pivot again to YQ-Metric-Series-Value for lags/means?
 
 #  Data
 
@@ -33,12 +20,10 @@ dat_yield =
   mutate(Year_Quarter = Year_Quarter %>% str_replace("_", "_Q")) %>% 
   pivot_longer(-Year_Quarter, names_to = "Species", values_to = "Yield")
 
-#   Note that adding supply data to covariates with bind_rows could make more sense. 
-
 dat_supply = 
   "03_intermediate/dat_notifications_1_9.csv" %>% 
   read_csv %>% 
-  # filter(Activity == "Clearcut/Overstory Removal") %>% 
+  # filter(Activity == "Clearcut/Overstory Removal") %>%
   select(
     Year_Quarter = QuarterCompletion,
     DouglasFir = MBF_2_DouglasFir,
@@ -53,41 +38,72 @@ dat_supply =
   group_by(Species) %>% 
   mutate(across(Supply, setNames(lapply(1:40, \(k) ~ lag(.x, k)), paste0("Lag_", 1:40)))) %>% 
   ungroup %>% 
-  rename(Supply_Lag_0 = Supply) %>% # Some covariates need this tweak as well. 
-  pivot_longer(starts_with("Supply"), names_to = "Series_Lag", values_to = "Value") %>% 
-  separate(Series_Lag, into = c("Series", "Lag"), sep = "_Lag_") %>% 
-  mutate(Lag = Lag %>% as.numeric %>% replace_na(0)) %>% 
-  pivot_wider(names_from = Lag, names_prefix = "Lag_", values_from = Value) %>% 
-  group_by(Series) %>% 
-  mutate(
-    Mean_4 = 
-      rowMeans(
-        pick(starts_with("Lag_") & ends_with(paste0("_", 1:4))),
-        na.rm = TRUE
-      ),
-    Mean_8 = 
-      rowMeans(
-        pick(starts_with("Lag_") & ends_with(paste0("_", 1:8))),
-        na.rm = TRUE
-      ),
-    Mean_12 = 
-      rowMeans(
-        pick(starts_with("Lag_") & ends_with(paste0("_", 1:12))),
-        na.rm = TRUE
-      )
+  rename(Supply_Lag_0 = Supply) %>% 
+  pivot_wider(
+    names_from = Species,
+    names_glue = "Supply_{Species}_{.value}", # This works but doesn't make any sense. 
+    values_from = starts_with("Supply")
   ) %>% 
-  ungroup
+  rename_with(~ str_remove(.x, "_Supply"))
 
-dat_covariates = 
+dat_variables = 
   "03_intermediate/dat_notifications_1_7.csv" %>% 
   read_csv %>% 
   select(-UID, -Year, -Quarter) %>% 
+  # Reduce across notifications.
   group_by(Year_Quarter) %>% 
   summarize(across(everything(), ~ mean(.x, na.rm = TRUE))) %>% 
   ungroup %>% 
+  # Add supply. 
+  left_join(dat_supply) %>% 
+  # Switch to long data for operations across series.
   pivot_longer(-Year_Quarter, names_to = "Series_Lag", values_to = "Value") %>% 
   separate(Series_Lag, into = c("Series", "Lag"), sep = "_Lag_") %>% 
   mutate(Lag = Lag %>% as.numeric %>% replace_na(0)) %>% 
+  filter(str_sub(Series, 1, 4) != "Fire") %>% # This is arbitrary. Note the same problem comes with CWD. 
+  # Compute first order differences.
+  rename(Value_Level = Value) %>% # For cleaner references. 
+  group_by(Series) %>% 
+  mutate(Value_FD = Value_Level - lag(Value_Level)) %>% 
+  ungroup %>% 
+  # Compute quarters for each lag. 
+  mutate(
+    Quarter = 
+      Year_Quarter %>% 
+      str_sub(-1, -1) %>% 
+      as.numeric %>% 
+      add(3) %>% 
+      subtract(((Lag - 1) %% 4))
+  ) %>% 
+  # Compute seasonal (quarterly) means by series for (un)differenced values.
+  group_by(Series, Quarter) %>% 
+  mutate(
+    Value_Level_Quarter_Mean = mean(Value_Level, na.rm = TRUE),
+    Value_FD_Quarter_Mean = mean(Value_FD, na.rm = TRUE)
+  ) %>% 
+  ungroup %>% 
+  # Compute demeaned level and change values. 
+  mutate(
+    Value_Level_SD = Value_Level - Value_Level_Quarter_Mean,
+    Value_FD_SD = Value_FD - Value_FD_Quarter_Mean
+  ) %>% 
+  # Switch to even longer (!) data.
+  select(
+    Year_Quarter, 
+    Series, 
+    Lag, 
+    Value_Level, 
+    Value_Level_SD, 
+    Value_FD, 
+    Value_FD_SD
+  ) %>% 
+  pivot_longer(
+    starts_with("Value"), 
+    names_prefix = "Value_", 
+    names_to = "Measure",
+    values_to = "Value"
+  ) %>% 
+  # Switch to wider data to compute (multi-)annual means. 
   pivot_wider(names_from = Lag, names_prefix = "Lag_", values_from = Value) %>% 
   group_by(Series) %>% 
   mutate(
@@ -125,31 +141,34 @@ vis_yield =
 #  Supply
 
 vis_supply = 
-  dat_supply %>% 
-  select(Year_Quarter, Species, Series, Mean_1 = Lag_1, Mean_4, Mean_8, Mean_12) %>% 
-  pivot_longer(starts_with("Mean"), names_to = "Metric", values_to = "Value") %>% 
-  mutate(Metric = Metric %>% factor %>% fct_relevel("Mean_1", "Mean_4", "Mean_8", "Mean_12")) %>%
-  ggplot() +
-  geom_line(aes(x = Year_Quarter, y = Value, color = Species, group = Species)) +
-  facet_wrap(~ Metric, nrow = 1)
-
-#  Prices | Douglas fir
-
-vis_price_douglasfir = 
-  dat_covariates %>% 
-  mutate(Keep = Series %>% str_detect("DouglasFir")) %>% 
-  filter(Keep) %>% 
-  select(Year_Quarter, Series, Mean_1 = Lag_1, Mean_4, Mean_8, Mean_12) %>% 
+  dat_variables %>% 
+  filter(Series %>% str_detect("Supply")) %>% 
+  select(Year_Quarter, Series, Measure, Mean_1 = Lag_1, Mean_4, Mean_8, Mean_12) %>% 
   pivot_longer(starts_with("Mean"), names_to = "Metric", values_to = "Value") %>% 
   mutate(Series = Series %>% str_remove("Price_") %>% str_remove("_DouglasFir"),
          Metric = Metric %>% factor %>% fct_relevel("Mean_1", "Mean_4", "Mean_8", "Mean_12")) %>%
   ggplot() +
   geom_line(aes(x = Year_Quarter, y = Value, color = Series, group = Series)) +
-  facet_wrap(~ Metric, nrow = 1)
+  facet_grid(Measure ~ Metric)
+
+#  Prices | Douglas fir
+
+vis_price_douglasfir = 
+  dat_variables %>% 
+  filter(Series %>% str_detect("DouglasFir") & Series %>% str_detect("Price")) %>% 
+  select(Year_Quarter, Series, Measure, Mean_1 = Lag_1, Mean_4, Mean_8, Mean_12) %>% 
+  pivot_longer(starts_with("Mean"), names_to = "Metric", values_to = "Value") %>% 
+  mutate(Series = Series %>% str_remove("Price_") %>% str_remove("_DouglasFir"),
+         Metric = Metric %>% factor %>% fct_relevel("Mean_1", "Mean_4", "Mean_8", "Mean_12")) %>%
+  ggplot() +
+  geom_line(aes(x = Year_Quarter, y = Value, color = Series, group = Series)) +
+  facet_grid(Measure ~ Metric)
 
 #  Prices | Western hemlock
 
 #  Prices | Composite
+
+
 
 #  Climate | CWD
 
