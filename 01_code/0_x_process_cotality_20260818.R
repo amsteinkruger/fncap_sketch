7a46373b88e2327352c7ed6c88ce4b16fcdc6b7a
Andrew Steinkruger <amsteinkruger@gmail.com>
2026-09-06 13:09:10 -0700

    Up to explicit-implicit panel transform.

--- 01_code/0_x_process_cotality_20260818.R
+++ 01_code/0_x_process_cotality_20260818.R
@@ -32,6 +32,17 @@ vec_pb_names_use =
   filter(SELECT == 1) %>% 
   pull(VARIABLE)
 
+#   Get even more useful variable names.
+
+vec_pb_names_bind = 
+  "02_data/0_0_0_Cotality/2_PB/OR_PB_SELECT.csv" %>% 
+  read_csv %>% 
+  filter(BIND == 1) %>% 
+  pull(VARIABLE) %>% 
+  str_replace_all(" ", "_")
+
+#   Read. 
+
 dat_pb = 
   "02_data/0_0_0_Cotality/2_PB/OR_PB_08052026.csv" %>% 
   read_csv(col_names = FALSE) %>% 
@@ -39,8 +50,6 @@ dat_pb =
   set_names(vec_pb_names_all) %>% 
   select(all_of(vec_pb_names_use)) %>% 
   rename_with(~ str_replace_all(.x, " ", "_"))
-
-dat_pb_problems = dat_pb %>% problems
 
 #  Get Owner Transfer data.
 
@@ -61,6 +70,15 @@ vec_ot_names_use =
   filter(SELECT == 1) %>% 
   pull(VARIABLE)
 
+#   Get even more useful variable names.
+
+vec_ot_names_bind = 
+  "02_data/0_0_0_Cotality/3_OT/OR_OT_SELECT.csv" %>% 
+  read_csv %>% 
+  filter(BIND == 1) %>% 
+  pull(VARIABLE) %>% 
+  str_replace_all(" ", "_")
+
 #   Get data, handle names, and select useful variables. 
 
 dat_ot = 
@@ -71,11 +89,7 @@ dat_ot =
   select(all_of(vec_ot_names_use)) %>% 
   rename_with(~ str_replace_all(.x, " ", "_"))
 
-#   Check problems. (They're fine.)
-
-dat_ot_problems = dat_ot %>% problems
-
-#  Reduce and visualize each dataset.  
+#  Visualize data and prepare for wrangling into an implicit panel.  
 
 #   Parcels
 
@@ -117,16 +131,7 @@ dat_pb_less_spatial %>%
   ggplot() + 
   geom_spatvector(color = "gray50", fill = NA, shape = 21, alpha = 0.25)
 
-#   Owner Transfers
-
-#    Reduce to Lane County.
-
-dat_ot_less = dat_ot %>% filter(FIPS_CODE == "41039")
-
-#    Since OT doesn't come with coordinates, skip ahead to the spatial join with PB. 
-#    (Then come back to a quick spatial plot.)
-
-#  Join PB to parcels by centroid nearest neighbors. 
+#   Join PB to parcels by centroid nearest neighbors. 
 
 dat_parcels_less_centroids = 
   dat_parcels_less %>% 
@@ -149,20 +154,40 @@ dat_pb_parcels =
   ) %>% 
   select(PARCEL, CLIP)
 
-#  Join OT to PB by CLIP. 
+#   Set up PB for an anti-join to OT and for appending to OT. 
 
-#    CLIP is unique in OT but not in PB. 
-#    There are no non-missing values in field PREVIOUS_CLIP for either PB or OT. 
-#    OWNER_TRANSFER_COMPOSITE_TRANSACTION_ID is a unique ID in OT. 
+dat_pb_bind = 
+  dat_pb_less %>% 
+  select(all_of(vec_pb_names_bind)) %>% 
+  semi_join(dat_pb_parcels)
 
-dat_pb_less_less = dat_pb_less %>% select(CLIP)
-dat_ot_less_less = dat_ot_less %>% select(CLIP, OWNER_TRANSFER_COMPOSITE_TRANSACTION_ID)
+#   Owner Transfers
 
-#   Join. 
+#    Reduce to Lane County.
 
-dat_pb_ot = dat_pb_less_less %>% left_join(dat_ot_less_less)
+dat_ot_less = dat_ot %>% filter(FIPS_CODE == "41039")
 
-#  Transform the OT-PB join into a panel of explicit and implicit ownership. 
+#    Set up OT for a semi-join to PB-Parcels and for appending to PB.  
+
+#     CLIP is unique in OT but not in PB. 
+#     There are no non-missing values in field PREVIOUS_CLIP for either PB or OT. 
+#     OWNER_TRANSFER_COMPOSITE_TRANSACTION_ID is a unique ID in OT. 
+
+dat_ot_bind = 
+  dat_ot_less %>% 
+  select(all_of(vec_ot_names_bind)) %>% 
+  rename_with(
+    ~ .x %>% 
+      str_trim %>% 
+      str_remove_all("DEED_") %>% 
+      str_remove_all("_-_STATIC") %>% 
+      str_replace_all("SALE_", "SALE_DERIVED_")
+  ) %>% 
+  semi_join(dat_pb_parcels)
+
+#   Prepare data in an implicit panel. 
+
+dat_pb_ot = dat_pb_bind %>% bind_rows(dat_ot_bind)
 
 #  ???
 
@@ -174,200 +199,7 @@ dat_pb_ot = dat_pb_less_less %>% left_join(dat_ot_less_less)
 #  Next piece with forest work is handling notification-PB/OT intersections with land use codes and landowner details
 #  So, work through handling covariates and subsetting parcels, then split workflows
 
-
-# Here be reference code. 
-
-library(haven)
-
-#  Owners
-
-#   With pre-processing in Stata to avoid variable type issues. 
-
-dat_owners = 
-  "02_data/0_0_0_Cotality/Owners/owners.csv" %>%
-  read_csv
-
-#   Note from problems() that columns 31 and 35 have difficult values.
-
-#  Parcels
-
-dat_parcels = 
-  "02_data/0_0_0_Cotality/Parcels/2020_shapefile/OR_parcels_2020.shp" %>% 
-  vect
-
-#  Spatial Join
-
-dat_parcels_less =
-  dat_parcels |> 
-  filter(County == 20) # %in% c(4, 29, 21, 20, 10, 6, 8)
-
-dat_parcels_less |>
-  slice_sample(n = 10000) |>
-  makeValid() |>
-  ggplot() +
-  geom_spatvector(aes(fill = Shape_Area), color = NA)
-
-# dat_parcels_summarize = 
-#   dat_parcels_less |> 
-#   makeValid() |> 
-#   summarize(Shape_Area = sum(Shape_Area)) |> 
-#   fillHoles()
-
-# owners to parcels by (1) intersection and (2) centroid nearest-neighbor
-
-dat_owners_less = dat_owners |> filter(ctyfips == 39) |> select(clip, starts_with("parcellevel"))
-
-dat_owners_less_spatial = 
-  dat_owners_less |> 
-  vect(geom = c("parcellevellongitude", "parcellevellatitude")) |> 
-  project("EPSG:3857")
-
-dat_owners_parcels_extract = terra::extract(dat_parcels_less, dat_owners_less_spatial)
-
-# Clean up for checks and joins.
-
-dat_owners_parcels_extract_less = 
-  dat_owners_parcels_extract |> 
-  select(ID_Parcel = OBJECTID,
-         ID_Owner = id.y)
-
-# Check.
-
-dat_owners_parcels_extract_less |> nrow() # 185168. Note multiple matches.
-dat_owners_parcels_extract_less |> drop_na(ID_Owner) |> nrow() # 185168
-dat_owners_parcels_extract_less |> drop_na(ID_Parcel) |> nrow() # 180578. So, 2.8% missing.
-
-dat_owners_parcels_extract_less |> 
-  group_by(ID_Owner) |> 
-  summarize(Count = n()) |> 
-  group_by(Count) |> 
-  summarize(Counter = n()) |> 
-  ungroup() |> 
-  arrange(desc(Counter))
-
-# 177385 owner records have only one parcel match. So, 4.2% of matches are problematic. 
-
-dat_owners_parcels_extract_less |> 
-  group_by(ID_Parcel) |> 
-  summarize(Count = n()) |> 
-  group_by(Count) |> 
-  summarize(Counter = n()) |> 
-  ungroup() |> 
-  arrange(desc(Counter))
-
-# 133874 of 154058 (87%) have only one owner match. Are multiple owner matches problematic? 
-
-# Reduce to 1-1 matches. 
-
-# dat_owners_parcels_clean = 
-#   dat_owners_parcels_extract_less |> 
-#   drop_na() |> 
-#   group_by(ID_Parcel) |> 
-#   mutate(Count_Parcel = n()) |> 
-#   group_by(ID_Owner) |> 
-#   mutate(Count_Owner = n()) |> 
-#   ungroup() |> 
-#   filter(Count_Parcel == 1) |> 
-#   filter(Count_Owner == 1)
-
-# 132902 (71%) of 185168 matches are 1-1. Centroid methods are a first suspect.
-
-# This is where matching on centroids rather than point-polygon extraction would go. 
-
-# Instead, reduce to the first match for each owner; preserve all owners.
-
-dat_owners_parcels_clean = 
-  dat_owners_parcels_extract_less |>
-  drop_na() |>
-  group_by(ID_Owner) |>
-  mutate(Number_Owner = row_number()) |>
-  ungroup() |>
-  filter(Number_Owner == 1) %>% 
-  select(starts_with("ID"))
-
-# Suppose these matches are fine, though. Go ahead with the join.
-
-dat_owners_parcels_join = 
-  dat_owners_parcels_clean |> 
-  left_join(dat_owners_less |> 
-              mutate(ID_Owner = row_number()) |> 
-              select(ID_Owner, clip)) |> 
-  select(-ID_Owner) %>% # magrittr pipe matters here. 
-  left_join(dat_parcels_less |> select(ID_Parcel = OBJECTID), .) |> 
-  drop_na(clip) |> 
-  left_join(dat_owners %>% select(clip, stateusedescription)) %>% 
-  select(clip, stateusedescription)
-
-# Check whether forest/timberland parcels fall within ODF's private forestry layer.
-
-dat_odf = 
-  "02_data/3_4_ODF_Ownership/Ownership.gdb" %>% 
-  vect %>% 
-  select(Owner = LandManager) %>% 
-  project("EPSG:3857") %>% 
-  group_by(Owner) %>% 
-  summarize %>% 
-  ungroup |> 
-  makeValid(buffer = TRUE)
-
-dat_owners_parcels_odf = 
-  dat_owners_parcels_join |> 
-  # slice_head(n = 100) |>
-  makeValid(buffer = TRUE) |> 
-  is.related(dat_odf, "intersects") |> 
-  tibble() |> 
-  rename(intersects = 1) %>%
-  bind_spat_cols(dat_owners_parcels_join, .) |> #  |> slice_head(n = 100)
-  filter(intersects)
-
-dat_owners_parcels_other = 
-  dat_owners_parcels_join |> 
-  # slice_head(n = 100) |>
-  anti_join(dat_owners_parcels_odf |> as_tibble() |> select(clip))
-
-# Pull values for land use codes in private forest/timberland. 
-
-dat_owners_parcels_odf |> 
-  as_tibble() |> 
-  group_by(stateusedescription) |> 
-  summarize(count = n()) |> 
-  ungroup() |> 
-  arrange(desc(count)) |> 
-  slice_head(n = 10)
-
-# Pull values for land use codes outside of forest/timberland.
-
-dat_owners_parcels_other |> 
-  as_tibble() |> 
-  group_by(stateusedescription) |> 
-  summarize(count = n()) |> 
-  ungroup() |> 
-  arrange(desc(count)) |> 
-  slice_head(n = 10)
-
-# Check top landowners (by counts of parcels) for forest/timberland and other land. 
-
-dat_owners_parcels_odf |> 
-  as_tibble() |> 
-  group_by(owner1fullname) |> 
-  summarize(count = n()) |> 
-  ungroup() |> 
-  arrange(desc(count)) |> 
-  slice_head(n = 10)
-
-dat_owners_parcels_other |> 
-  as_tibble() |> 
-  group_by(owner1fullname) |> 
-  summarize(count = n()) |> 
-  ungroup() |> 
-  arrange(desc(count)) |> 
-  slice_head(n = 10)
-
-# Join transfers onto ownership and parcels to obtain a panel of ownership. 
-
-dat_transactions = 
-  "02_data/0_0_0_Cotality/Transactions/Lane_Res_clean_v2026.dta" |> 
-  read_dta()
+# reference code follows
 
 dat_transactions_less =
   dat_transactions |> 
@@ -379,7 +211,7 @@ dat_transactions_spatial =
   select(clip, starts_with("parcel_")) |> 
   vect(geom = c("parcel_longitude", "parcel_latitude")) |> 
   project("EPSG:3857")
-  
+
 dat_owners_transactions_extract = 
   dat_owners_parcels_join |> 
   select(clip) |> 
@@ -468,5 +300,3 @@ dat_panel_filled =
   mutate(owner = owner_fill,
          which = ifelse(is.na(which), "inferred", which)) |> 
   select(-c(owner_combine, owner_fill))
-
-#  Export
